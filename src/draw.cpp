fa1a70c4489e0d1c4e86e56d6cb6f43729cbe7fb
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2022-01-23 09:48:21 +0300

    Add update member-function for class Draw

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,18 +1,53 @@
 #include "draw.hpp"
 
-Draw::Draw(Window &window)
-: glfw_window(window.get_glfwwindow()) {
+#include "exception.hpp"
 
-}
+Draw::Draw(Window &window, Vbo &_position_buffer_object,
+ const VertAttrArr &_vert_attr_arr, const ShaderProgram &_shader_program)
+: glfw_window(window.get_glfwwindow())
+, position_buffer_object(&_position_buffer_object)
+, vert_attr_arr(&_vert_attr_arr)
+, shader_program(&_shader_program) {}
 
 Draw::~Draw() {
 
 }
 
-Draw::clear() {
+void Draw::clear() {
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-Draw::update() {
+void Draw::fill(const Color &color) {
+    glClearColor(color.get_r(), color.get_g(), color.get_b(), color.get_a());
+}
+
+void Draw::update() {
+    GLenum error;
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    shader_program->use();
+
+    position_buffer_object->bind();
+    position_buffer_object->set_vertices(vertices, sizeof(float) * 9);
+    vert_attr_arr->enable();
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    error = glGetError();
+    if (error != GL_NO_ERROR)
+        ANG_THROW(reinterpret_cast<const char *>(gluErrorString(error)));
+
+    vert_attr_arr->disable();
+    position_buffer_object->unbind();
+    shader_program->unuse();
+
     glfwSwapBuffers(glfw_window);
 }
--- src/draw.hpp
+++ src/draw.hpp
@@ -1,18 +1,31 @@
 #ifndef DRAW_HPP
 #define DRAW_HPP
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include "color.hpp"
+#include "vbo.hpp"
+#include "shader_program.hpp"
+#include "vertex_attribute_array.hpp"
+#include "window.hpp"
 
 class Draw {
     private:
-        GLFWwindow* glfw_window;
+        GLFWwindow          *glfw_window;
+        Vbo                 *position_buffer_object;
+        const VertAttrArr   *vert_attr_arr;
+        const ShaderProgram *shader_program;
 
     public:
-        Draw(Window &window);
+        Draw(Window &window, Vbo &_position_buffer_object,
+         const VertAttrArr &_vert_attr_arr,
+         const ShaderProgram &_shader_program);
         ~Draw();
 
-        clear();
-        update();
+        void clear();
+        void fill(const Color &color);
+        void update();
 };
 
 #endif // KEYBOARD_HPP
