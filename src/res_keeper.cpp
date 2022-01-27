6f3776c3ab50055c016c3a460e764b7248d0babe
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2022-01-27 07:01:15 +0300

    Add checking if sprite with name available

--- src/res_keeper.cpp
+++ src/res_keeper.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+#include "exception.hpp"
 #include "iimg.hpp"
 #include "img_characters.hpp"
 #include "img_floor.hpp"
@@ -12,26 +13,26 @@ ResKeeper::ResKeeper()
 : textures()
 , spritesheets()
 , sprites() {
-    std::unique_ptr<IImg> img_characters = std::make_unique<ImgCharacters>();
     std::unique_ptr<IImg> img_floor = std::make_unique<ImgFloor>();
+    std::unique_ptr<IImg> img_characters = std::make_unique<ImgCharacters>();
+    Texture              *tex_floor = new Texture(img_floor->get_data(),
+     img_floor->get_width(), img_floor->get_height());
     Texture              *tex_characters = new Texture(
      img_characters->get_data(), img_characters->get_width(),
      img_characters->get_height());
-    Texture              *tex_floor = new Texture(img_floor->get_data(),
-     img_floor->get_width(), img_floor->get_height());
+    Spritesheet          *sprsh_floor = new Spritesheet(*tex_floor, 128, 128);
     Spritesheet          *sprsh_characters = new Spritesheet(*tex_characters,
      16, 16);
-    Spritesheet          *sprsh_floor = new Spritesheet(*tex_floor, 128, 128);
+    Sprite               *spr_floor = new Sprite(*sprsh_floor, 0);
     Sprite               *spr_boy = new Sprite(*sprsh_characters, 0);
     Sprite               *spr_girl = new Sprite(*sprsh_characters, 3);
-    Sprite               *spr_floor = new Sprite(*sprsh_floor, 0);
 
-    textures.insert({tex_characters, tex_floor});
-    spritesheets.insert({sprsh_characters, sprsh_floor});
+    textures.insert({tex_floor, tex_characters});
+    spritesheets.insert({sprsh_floor, sprsh_characters});
     sprites.insert({
+        {"floor"s, spr_floor},
         {"boy"s, spr_boy},
         {"girl"s, spr_girl},
-        {"floor"s, spr_floor},
     });
 }
 
@@ -45,5 +46,10 @@ ResKeeper::~ResKeeper() {
 }
 
 Sprite *ResKeeper::get_sprite(const std::string &name) {
-    return sprites[name];
+    Sprite *sprite = sprites[name];
+
+    if (!sprite)
+        ANG_THROW("Sprite not found: "s.append(name).c_str());
+
+    return sprite;
 }
